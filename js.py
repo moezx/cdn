32d56e39c2bf59dad23180255ecfdeb586f41f62
Mashiro <adadam@qq.com>
2018-04-12 23:25:24 +0800

    4/12

--- js.py
+++ js.py
@@ -2,18 +2,35 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-path = 'js/'
+pathJS = 'js/'
+pathCSS = 'css/src/'
+pathCSSroot = 'css/'
 
+jsfiles = [f for f in listdir(pathJS) if isfile(join(pathJS, f))]
+cssfiles = [f for f in listdir(pathCSS) if isfile(join(pathCSS, f))]
 
-onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+strJS = ''
+strCSS = ''
 
-str = ''
-
-for f in onlyfiles:
-    with open(path + f, 'r') as file:
+for f in jsfiles:
+    with open(pathJS + f, 'r') as file:
         data = file.read()
-    str = str + data
+    strJS = strJS + data
     print(f)
         
 with open("lib.js", "w") as text_file:
-    print(str, file=text_file)
+    print(strJS, file=text_file)
+    
+    
+print('CSS############################')
+    
+for f in cssfiles:
+    with open(pathCSS + f, 'r') as file:
+        data = file.read()
+    strCSS = strCSS + data
+    print(f)
+        
+with open("css/lib.css", "w") as text_file:
+    print(strCSS, file=text_file)
+    
+print('Done')
