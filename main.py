4b6bcd43125ceecdc959377789014fe71d772d06
Mashiro <adadam@qq.com>
2018-04-12 20:37:24 +0800

    4/12

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+path = 'js/'
+
+
+onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+str = ''
+
+for f in onlyfiles:
+    with open(path + f, 'r') as file:
+        data = file.read()
+    str = str + data
+        
+with open("lib.js", "w") as text_file:
+    print(str, file=text_file)
