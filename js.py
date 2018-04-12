26dc93237a0b8475adac45153ef9b6782a0087d0
Mashiro <adadam@qq.com>
2018-04-12 22:12:33 +0800

    4/12

--- js.py
+++ js.py
@@ -13,6 +13,7 @@ for f in onlyfiles:
     with open(path + f, 'r') as file:
         data = file.read()
     str = str + data
+    print(f)
         
 with open("lib.js", "w") as text_file:
     print(str, file=text_file)
